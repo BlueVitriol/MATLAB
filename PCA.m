18247ac66eddd84fba0ff541d7b52cd0441a8c12
BlueVitriol <sarah.li@uqconnect.edu.au>
2014-03-24 19:30:33 +1000

    Update PCA.m

--- PCA.m
+++ PCA.m
@@ -1,7 +1,6 @@
-
 function [z, V, PC] = PCA(data, dim)
 
-% SYNOPOSIS:
+% SYNOPSIS:
 % Principal Component Analysis (PCA) is a statistical procedure that uses 
 % orthogonal transformation to convert a set of observations of possibly 
 % correlated variables into a set of values of linearly uncorrelated variables 
